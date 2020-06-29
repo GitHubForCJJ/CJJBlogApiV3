e800436c81c4fe4f03f478c1de8c7e350abba042
chenjianjun <chenjianjun@567na.com>
2020-06-29 14:37:45 +0800

    serilog

--- CJJ_BlogApiV3/Controllers/BlogController.cs
+++ CJJ_BlogApiV3/Controllers/BlogController.cs
@@ -9,6 +9,8 @@ using CJJ_BlogApiV3.Model.DbBaseModel;
 using CJJ_BlogApiV3.Model.ReuqestModel;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace CJJ_BlogApiV3New.Controllers
 {
@@ -20,9 +22,11 @@ namespace CJJ_BlogApiV3New.Controllers
     public class BlogController : ControllerBase
     {
         private readonly IBloginfoLogic bloginfoLogic = null;
-        public BlogController(IBloginfoLogic bloginfoLogic)
+        private readonly ILogger<BlogController> logger;
+        public BlogController(IBloginfoLogic bloginfoLogic,ILogger<BlogController> _logger)
         {
             this.bloginfoLogic = bloginfoLogic;
+            logger = _logger;
         }
         /// <summary>
         /// 查询list
@@ -34,15 +38,19 @@ namespace CJJ_BlogApiV3New.Controllers
         {
             try
             {
-                LogManager.Info("GetList进入22  info");
-                LogManager.Error("GetList进入  errorinfo");
+                //LogManager.Info("GetList进入22  info");
+                //LogManager.Error("GetList进入  errorinfo");
+                logger.LogDebug("debug message");
+                logger.LogInformation("Seeding database...");
                 int a = 1, b = 0;
-                var c = a / b;
+                int c=a / b;
                 var list = await bloginfoLogic.GetListBloginfo(model);
                 return new JsonResponse { Code = 0, Data = list };
             }
             catch (Exception e)
             {
+                logger.LogError(e, "BlogController/GetList");
+                //Log.Error(e, "BlogController/GetList");
                 LogManager.Error("BlogController/GetList" + e.Message, e);
                 return new JsonResponse { Code = 1, Msg = "程序错误" + e.Message };
             }
--- CJJ_BlogApiV3/Program.cs
+++ CJJ_BlogApiV3/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Autofac.Extensions.DependencyInjection;
@@ -7,6 +8,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace CJJ_BlogApiV3
 {
@@ -19,9 +22,62 @@ namespace CJJ_BlogApiV3
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog((ctx, config) =>
+                {
+                    config.MinimumLevel.Debug()
+                        .MinimumLevel.Debug()
+                        .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                        .MinimumLevel.Override("System", LogEventLevel.Warning)
+                        .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
+                        .Enrich.FromLogContext();
+
+                    if (ctx.HostingEnvironment.IsDevelopment())
+                    {
+                        //config.WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}");
+                        config.WriteTo.File($"{AppDomain.CurrentDomain.BaseDirectory}LogFiles{Path.DirectorySeparatorChar}cjjwebapiv3console.txt",
+                        fileSizeLimitBytes: 1_000_000,
+                        rollOnFileSizeLimit: true,
+                        shared: true,
+                        flushToDiskInterval: TimeSpan.FromSeconds(1));
+                    }
+                    else if (ctx.HostingEnvironment.IsProduction())
+                    {
+                        config.WriteTo.File($"{AppDomain.CurrentDomain.BaseDirectory}{Path.DirectorySeparatorChar}LogFiles{{Path.DirectorySeparatorChar}}cjjwebapiv3.txt",
+                            fileSizeLimitBytes: 1_000_000,
+                            rollOnFileSizeLimit: true,
+                            shared: true,
+                            flushToDiskInterval: TimeSpan.FromSeconds(1));
+                    }
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseSerilog((hostingContext, loggerConfiguration) =>
+                    {
+
+                        string LogFilePath(string LogEvent) => $@"{AppDomain.CurrentDomain.BaseDirectory}LogFiles\{LogEvent}\log.log";
+                        //模板
+                        string SerilogOutputTemplate = "{NewLine}{NewLine}Date：{Timestamp:yyyy-MM-dd HH:mm:ss.fff}{NewLine}LogLevel：{Level}{NewLine}Message：{Message}{NewLine}{Exception}" + new string('-', 50);
+
+                        loggerConfiguration
+                        .MinimumLevel.Debug()
+                        .MinimumLevel.Override("Microsoft", LogEventLevel.Debug)
+                        .MinimumLevel.Override("System", LogEventLevel.Debug)
+                        .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
+                        .Enrich.FromLogContext()
+
+                       .ReadFrom.Configuration(hostingContext.Configuration)
+                       .Enrich.FromLogContext()
+                       //.WriteTo.File($"{AppDomain.CurrentDomain.BaseDirectory}{Path.DirectorySeparatorChar}LogFiles{Path.DirectorySeparatorChar}cjjapi.txt", rollingInterval: RollingInterval.Day)
+                       //不同类型的日志写到不同的文件夹中
+                       .WriteTo.Logger(lg => lg.Filter.ByIncludingOnly(p => p.Level == LogEventLevel.Debug).WriteTo.File(LogFilePath("Debug"), rollingInterval: RollingInterval.Day, outputTemplate: SerilogOutputTemplate))
+.WriteTo.Logger(lg => lg.Filter.ByIncludingOnly(p => p.Level == LogEventLevel.Information).WriteTo.File(LogFilePath("Information"), rollingInterval: RollingInterval.Day, outputTemplate: SerilogOutputTemplate))
+.WriteTo.Logger(lg => lg.Filter.ByIncludingOnly(p => p.Level == LogEventLevel.Warning).WriteTo.File(LogFilePath("Warning"), rollingInterval: RollingInterval.Day, outputTemplate: SerilogOutputTemplate))
+.WriteTo.Logger(lg => lg.Filter.ByIncludingOnly(p => p.Level == LogEventLevel.Error).WriteTo.File(LogFilePath("Error"), rollingInterval: RollingInterval.Day, outputTemplate: SerilogOutputTemplate))
+.WriteTo.Logger(lg => lg.Filter.ByIncludingOnly(p => p.Level == LogEventLevel.Fatal).WriteTo.File(LogFilePath("Fatal"), rollingInterval: RollingInterval.Day, outputTemplate: SerilogOutputTemplate))
+
+                       .WriteTo.Console();
+                    });
                 }).UseServiceProviderFactory(new AutofacServiceProviderFactory());//替换原有的IOC容器
     }
 }
